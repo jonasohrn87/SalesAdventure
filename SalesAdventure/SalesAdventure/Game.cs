31c997fb478aa3026066d2c648cd56c074ba9637
Jonas Öhrn <theeagle87@hotmail.com>
2024-02-08 15:30:09 +0100

    map and players

--- SalesAdventure/SalesAdventure/Game.cs
+++ SalesAdventure/SalesAdventure/Game.cs
@@ -1,37 +1,102 @@
-﻿//using System;
-//using SalesAdventure;
-////using Game;
-
-
-using SalesAdventure;
+﻿using System;
 using System.Threading.Channels;
 
-internal class Game
+class Game
 {
+    string[,] map = new string[10, 10];
+    int playerPosX = 5;
+    int playerPosY = 5;
+    string player = "P";
+    //string playerSymbol = "P";
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+    bool runGame = true;
+
+
     public Game()
     {
     }
-        //const int width = 10;
-        //const int height = 10;
-        //public string[,] worldMap { get; } = new string[width, height];
 
     public void Run()
     {
-        //Game gameWorld = new Game();
-        string[,] worldMap = new string[10, 10];
-        worldMap[2, 3] = "2";
+        DrawMap();
+        PlacePlayer();
 
-        Console.WriteLine(" h e j");
-        for (int i = 0; i < worldMap.GetLength(0); i++)
+        Console.WriteLine("Welcome Player!!");
+
+        //while (runGame)
+        //{
+
+        FillMap();
+
+        //    readkey
+
+        //    up
+        //    MovePlayer();
+
+        //    down
+        //    MovePlayer();
+
+        //    left
+        //    MovePlayer();
+
+        //    right
+        //    MovePlayer();
+
+        //}
+
+
+    }
+
+    //private void MovePlayer(int playerPosX, int playerPosY, int i, int j)
+    //{
+    //    if ((keyInfo.Key == ConsoleKey.UpArrow))//Lägga till logik för träffa väggar åt alla hållen
+    //    {
+    //        playerPosY = j++;
+    //    }
+    //    else if (keyInfo.Key == ConsoleKey.DownArrow)
+    //    {
+    //        playerPosY = j--;
+    //    }
+    //    else if (keyInfo.Key == ConsoleKey.LeftArrow)
+    //    {
+    //        playerPosY = i--;
+    //    }
+    //    else if (keyInfo.Key == ConsoleKey.RightArrow)
+    //    {
+    //        playerPosY = i++;
+    //    }
+    //    else
+    //    {
+    //        runGame = false;
+    //    }
+    //}
+
+    public void PlacePlayer()
+    {
+        map[playerPosX, playerPosY] = player;
+    }
+
+
+    public void DrawMap()
+    {
+        for (int i = 0; i < 10; i++)
         {
-            for (int j = 0; j < worldMap.GetLength(1); j++)
+            for (int j = 0; j < 10; j++)
             {
-                Console.Write(worldMap[i, j] + " - ");
+                map[i, j] = ".";
+
+            }
+        }
+    }
+    public void FillMap()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(map[i, j] + " ");
             }
             Console.WriteLine();
         }
-
-
-        Console.ReadLine();
     }
 }
