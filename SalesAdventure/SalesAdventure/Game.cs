c14113f6fee2a0d85e347e883853e2b76e0a6520
o7Bane <o7bane@outlook.com>
2024-02-12 08:21:58 +0100

    monster encounter, player movement update

--- SalesAdventure/SalesAdventure/Game.cs
+++ SalesAdventure/SalesAdventure/Game.cs
@@ -27,28 +27,23 @@ namespace SalesAdventure
         {
             Map.DrawMap drawMap = new Map.DrawMap(map, mapSizeX, mapSizeY);
 
-            
             Console.WriteLine("Welcome Player!!\nPress Enter to play the SalesAdventures\n");
             Console.ReadLine();
 
-            
-            //Console.Clear();
             drawMap.Fill();
             PlacePlayer();
             SpawnEnemies();
-             
-
-            //Console.ReadLine();
 
             while (runGame)
             {
                 Console.Clear();
                 Console.WriteLine("Use Arrows or WASD to Move around or press Q to Quit\n");
+
                 drawMap.Draw();
                 drawMap.Fill();
                 PlacePlayer();
                 SpawnEnemies();
-
+                MonsterEncounter();
 
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
 
@@ -56,22 +51,22 @@ namespace SalesAdventure
                 {
                     case ConsoleKey.UpArrow:
                     case ConsoleKey.W:
-                        MovePlayer("W");
+                        MovePlayer(-1, 0);
                         break;
 
                     case ConsoleKey.DownArrow:
                     case ConsoleKey.S:
-                        MovePlayer("S");
+                        MovePlayer(1, 0);
                         break;
 
                     case ConsoleKey.LeftArrow:
                     case ConsoleKey.A:
-                        MovePlayer("A");
+                        MovePlayer(0, -1);
                         break;
 
                     case ConsoleKey.RightArrow:
                     case ConsoleKey.D:
-                        MovePlayer("D");
+                        MovePlayer(0, 1);
                         break;
 
                     case ConsoleKey.Q:
@@ -84,67 +79,84 @@ namespace SalesAdventure
             }
         }
 
-        private void SpawnEnemies()
+        private void MonsterEncounter()
         {
-            map[monsterPosY, monsterPosX] = monster;
-        }
+            if (monsterPosY == playerPosY && monsterPosX == playerPosX && monster == "M")
+            {
+                Console.Clear();
+                Console.WriteLine("You've encountered an Orge Giant, what will you do?");
+                Console.WriteLine(" A. Attack!!\n F. Flee encounter!");
 
-        private void MovePlayer(string direction)
-        {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
 
-            if (direction == "W")
-            {
+                if (keyInfo.Key == ConsoleKey.A)
                 {
-                    map[playerPosY, playerPosX] = ".";
-                    playerPosY--;
-                    PlacePlayer();
+                    bool monsterFight = true;
+
+                    while (monsterFight)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("PlayerHealth: \"100\" MonsterHealth: \"80\"");
+                        Console.WriteLine(" 1. Attack\n 2. Block\n 3. Flee");
+                        keyInfo = Console.ReadKey();
+                        switch (keyInfo.Key)
+                        {
+                            case ConsoleKey.D1:
+                                Console.WriteLine("Hitting monster");
+                                monster = ".";
+                                break;
+
+                            case ConsoleKey.D2:
+                                Console.WriteLine("Getting hit");
+                                break;
+
+                            case ConsoleKey.D3:
+                                monsterFight = false;
+                                playerPosX--;
+                                PlacePlayer();
+                                break;
+
+                            default:
+                                break;
+                        }
+                        if (monster == ".")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("You WON! Press any key to Continue.");
+                            monsterFight = false;
+                            PlacePlayer();
+                        }
+                    }
                 }
-            }
-            else if (direction == "S")
-            {
+                else if (keyInfo.Key == ConsoleKey.F)
                 {
-                    map[playerPosY, playerPosX] = ".";
-                    playerPosY++;
-                    PlacePlayer();
-                }
-            }
-            else if (direction == "A")
-            {
-                {
-                    map[playerPosY, playerPosX] = ".";
                     playerPosX--;
                     PlacePlayer();
                 }
             }
-            else if (direction == "D")
+        }
+
+        private void SpawnEnemies()
+        {
+            map[monsterPosY, monsterPosX] = monster;
+        }
+        private void MovePlayer(int movePosY, int movePosX)
+        {
+            int newPosY = playerPosY + movePosY;
+            int newPosX = playerPosX + movePosX;
+
+            if (newPosY > 0 && newPosY < mapSizeY - 1 && newPosX > 0 && newPosX < mapSizeX - 1)
             {
-                {
-                    map[playerPosY, playerPosX] = ".";
-                    playerPosX++;
-                    PlacePlayer();
-                }
+                map[playerPosY, playerPosX] = ".";
+                playerPosY = newPosY;
+                playerPosX = newPosX;
+                PlacePlayer();
             }
         }
 
-        public void PlacePlayer()
+            public void PlacePlayer()
         {
-            if (playerPosY <= 0)
-            {
-                playerPosY++;
-            }
-            else if (playerPosY >= 11)
-            {
-                playerPosY--;
-            }
-            else if (playerPosX <= 0)
-            {
-                playerPosX++;
-            }
-            else if (playerPosX >= 11)
-            {
-                playerPosX--;
-            }
             map[playerPosY, playerPosX] = player;
-        }       
+        }
     }
 }
